18c90579f01ee6dbe5143d62fb1625e6cdb2bc88
Emmanuel Tsyawo <emmantsygarv@hotmail.com>
2019-02-07 21:07:03 -0500

    edit, matply.c, update with functions

--- system_linear_equations/matply.h
+++ system_linear_equations/matply.h
@@ -7,6 +7,9 @@
 
 // declare function prototypes.
 void matply(double *xa, double *xb,double *xab, int *nra, int *nca,int *ncb);
+void matply_sym(double *x, double *xx, int *nrx, int *ncx);
+void matply_xty(double *x, double *y, double *xty, int *nrx, int *ncx, int *ncy);
+void matply_xyt(double *x, double *y, double *xyt, int *nrx, int *ncx, int *nry);
 void trans(double *a, double *atrans,int *nra,int *nca);
 double dotprod(double *a, double *b, int *n);
 double max(double *x, int *n);
